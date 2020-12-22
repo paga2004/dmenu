385c3dfef61c7e45a6f0bd27f89ae197b64bebcc
paga2004 <pascigamma@bluewin.ch>
2020-12-22 18:03:20 +0100

    Change colors

--- config.h
+++ config.h
@@ -5,14 +5,14 @@ static int topbar = 0;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
+    "NotoColorEmoji:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#839496", "#002b36" },
-	[SchemeSel] = { "#839496", "#073642"},
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#586e75", "#002b36" },
+	[SchemeSel] = { "#93a1a1", "#073642"},
+	[SchemeOut] = { "#002b36", "#586e75" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
